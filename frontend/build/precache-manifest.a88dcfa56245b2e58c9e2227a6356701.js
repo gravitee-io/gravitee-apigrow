3faf7dfdc163fdd9f459cfa4e3983fb2ffb4da2d
Apizr <github@apizr.fr>
2020-08-28 10:48:07 +0000

    chore: upgrade apigrow binaries to 1.15.0

---
+++ frontend/build/precache-manifest.a88dcfa56245b2e58c9e2227a6356701.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = [
+  {
+    "revision": "f68f05d38364a811dae6",
+    "url": "/static/css/0.feea6c47.chunk.css"
+  },
+  {
+    "revision": "f68f05d38364a811dae6",
+    "url": "/static/js/0.f68f05d3.chunk.js"
+  },
+  {
+    "revision": "761c26c55c3a6e28b76a",
+    "url": "/static/css/1.5d6db98a.chunk.css"
+  },
+  {
+    "revision": "761c26c55c3a6e28b76a",
+    "url": "/static/js/1.761c26c5.chunk.js"
+  },
+  {
+    "revision": "96fea96f5e6cd02dccdd",
+    "url": "/static/js/2.96fea96f.chunk.js"
+  },
+  {
+    "revision": "0995d04f48dfd98d65a0",
+    "url": "/static/js/3.0995d04f.chunk.js"
+  },
+  {
+    "revision": "363c1fbe182ea30fb6f0",
+    "url": "/static/js/4.363c1fbe.chunk.js"
+  },
+  {
+    "revision": "af64893a6669e28c0bb8",
+    "url": "/static/css/main.176bbc78.chunk.css"
+  },
+  {
+    "revision": "af64893a6669e28c0bb8",
+    "url": "/static/js/main.af64893a.chunk.js"
+  },
+  {
+    "revision": "0142fb636e467671303f",
+    "url": "/static/js/runtime~main.0142fb63.js"
+  },
+  {
+    "revision": "4fe1ecac44f2205ea268",
+    "url": "/static/css/7.51d57be2.chunk.css"
+  },
+  {
+    "revision": "4fe1ecac44f2205ea268",
+    "url": "/static/js/7.4fe1ecac.chunk.js"
+  },
+  {
+    "revision": "b04e96b33c2fc0db7111",
+    "url": "/static/js/8.b04e96b3.chunk.js"
+  },
+  {
+    "revision": "50e02e807550f9d5a34e",
+    "url": "/static/js/9.50e02e80.chunk.js"
+  },
+  {
+    "revision": "cad0c17ebd1f260ed1f5",
+    "url": "/static/js/10.cad0c17e.chunk.js"
+  },
+  {
+    "revision": "1d12500caa9759903229",
+    "url": "/static/js/11.1d12500c.chunk.js"
+  },
+  {
+    "revision": "efa2b5a176b6ff0d4014",
+    "url": "/static/js/12.efa2b5a1.chunk.js"
+  },
+  {
+    "revision": "61038164386bc8138353",
+    "url": "/static/js/13.61038164.chunk.js"
+  },
+  {
+    "revision": "a2fd8bea3f5b18c506f1",
+    "url": "/static/js/14.a2fd8bea.chunk.js"
+  },
+  {
+    "revision": "825b96a489616e426ea8",
+    "url": "/static/js/15.825b96a4.chunk.js"
+  },
+  {
+    "revision": "3dc9039dd9b9d5b81cd8",
+    "url": "/static/css/16.cb3c2e6a.chunk.css"
+  },
+  {
+    "revision": "3dc9039dd9b9d5b81cd8",
+    "url": "/static/js/16.3dc9039d.chunk.js"
+  },
+  {
+    "revision": "2d9d0bbd9a9384b31126",
+    "url": "/static/css/17.7ac5d7a2.chunk.css"
+  },
+  {
+    "revision": "2d9d0bbd9a9384b31126",
+    "url": "/static/js/17.2d9d0bbd.chunk.js"
+  },
+  {
+    "revision": "17af5e899ff34527fc1c",
+    "url": "/static/js/18.17af5e89.chunk.js"
+  },
+  {
+    "revision": "a2ebbe2c4994eaa86bf1",
+    "url": "/static/js/19.a2ebbe2c.chunk.js"
+  },
+  {
+    "revision": "37cd83ae0d2a95708b58",
+    "url": "/static/js/20.37cd83ae.chunk.js"
+  },
+  {
+    "revision": "a43e8d2e1fcf37a82d6d",
+    "url": "/static/js/21.a43e8d2e.chunk.js"
+  },
+  {
+    "revision": "40b28f3b79dc1199ae2f",
+    "url": "/static/js/22.40b28f3b.chunk.js"
+  },
+  {
+    "revision": "63b54a9736bd672bc107",
+    "url": "/static/js/23.63b54a97.chunk.js"
+  },
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "/static/media/element-icons.d2f69a92.woff"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "/static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "1de2c84af88797f251e64256de2883bf",
+    "url": "/static/media/credits-4.1de2c84a.png"
+  },
+  {
+    "revision": "c6a3fe1d3003e3eade4ba46ac78d7592",
+    "url": "/static/media/secured.c6a3fe1d.jpg"
+  },
+  {
+    "revision": "ffe3c0801c94572f17ecf08a0c309caf",
+    "url": "/static/media/credit-cards.ffe3c080.png"
+  },
+  {
+    "revision": "f61dcb978450509df95f4203e99000c1",
+    "url": "/static/media/cross_red_circle.f61dcb97.svg"
+  },
+  {
+    "revision": "46570f0809131134c01dc096e50739fc",
+    "url": "/static/media/check_green_circle.46570f08.svg"
+  },
+  {
+    "revision": "5c6b980185574036d91ba12f104a6d49",
+    "url": "/static/js/9.50e02e80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "045b96f6cf996eca89d392677e26d104",
+    "url": "/static/js/7.4fe1ecac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcbbbe32275316b4050cd0b5f82255f2",
+    "url": "/static/js/1.761c26c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7f6e14b7734049f5c63edebe1d63ff8",
+    "url": "/index.html"
+  }
+];
--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a917a77c43ee0eff68893f8765fd9b6c.js"
+  "/precache-manifest.a88dcfa56245b2e58c9e2227a6356701.js"
 );
 
 workbox.clientsClaim();
