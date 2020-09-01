719061872f8d76b949f7ea38df8b7edfe955fdc4
Apizr <github@apizr.fr>
2020-09-01 14:43:10 +0000

    chore: upgrade apigrow binaries to 1.16.0

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a88dcfa56245b2e58c9e2227a6356701.js"
+  "/precache-manifest.4e1ca58c5f2880cf82ba165a99a6b330.js"
 );
 
 workbox.clientsClaim();
